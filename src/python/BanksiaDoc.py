0b038b27e83cfaf3cf2af0966182e3288458c9c6
Mariia Nesterenko <115986423+bg-mn@users.noreply.github.com>
2024-04-30 11:38:57 +0700

    add postman collection

--- src/python/BanksiaDoc.py
+++ src/python/BanksiaDoc.py
@@ -5,8 +5,9 @@ import docx
 import pytesseract
 from PIL import Image
 import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-OPENAI_API_KEY: Final = os.getenv("OPENAI_API_KEY", "")
+OPENAI_API_KEY: Final = os.getenv("OPENAI_API_KEY", "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")
 
 class DocumentProcessor:
     def __init__(self, api_key: str) -> None:
@@ -45,16 +46,27 @@ class DocumentProcessor:
         return pytesseract.image_to_string(Image.open(image_path))
 
     def embed_text(self, text: str) -> List[Tuple[float]]:
+        # text = "Kaiser Foundation Health Plan, Inc.\nNorthern and Southern California Regions\nA nonprofit corporation\n2024 Combined Membership Agreement,\nEvidence of Coverage, and Disclosure Form for\nKaiser Permanente for Individuals and Families\nKaiser Permanente - Gold 80 HMO\nA plan for members who enroll through Covered California or\ndirectly with Kaiser Permanente\nMember Services\n24 hours a day, seven days a week (except closed holidays)\n1‑800-464-4000 (TTY users call 711)\nkp.org\n800111-01-18-03.\n.coaccum NGF ACA p16142TABLE OF CONTENTS\nCost Share Summary ..............................................................................................................................................................1\nAccumulation Period ..........................................................................................................................................................1\nDeductibles and Out-of-Pocket Maximums .......................................................................................................................1\nCost Share Summary Tables by Benefit .............................................................................................................................1\nCARE Plan .......................................................................................................................................................................17\nIntroduction ..........................................................................................................................................................................19\nAbout Kaiser Permanente .................................................................................................................................................19\nTerm of this EOC, Renewal, and Amendment .................................................................................................................19\nDefinitions ............................................................................................................................................................................20\nPremiums, Eligibility, and Enrollment .................................................................................................................................27\nPremiums ..........................................................................................................................................................................27\nWho Is Eligible .................................................................................................................................................................27\nHow to Enroll and When Coverage Begins .....................................................................................................................30\nHow to Obtain Services ........................................................................................................................................................31\nRoutine Care .....................................................................................................................................................................31\nUrgent Care ......................................................................................................................................................................31\nNot Sure What Kind of Care You Need? .........................................................................................................................31\nYour Personal Plan Physician ..........................................................................................................................................31\nGetting a Referral .............................................................................................................................................................32\nTravel and Lodging for Certain Services .........................................................................................................................34"
         return self.embeddings.embed_documents([text])
     
 
 def start(path: str) -> None:
     processor = DocumentProcessor(OPENAI_API_KEY)
-    document_vector = processor.process_document(path)
-    return document_vector
+    text_splitter = RecursiveCharacterTextSplitter(length_function=len,
+    is_separator_regex=False,)
+    state_of_the_union = processor._extract_text_from_pdf(path)
+    
+    texts = text_splitter.create_documents([state_of_the_union])
+    for key, text in enumerate(texts):
+        processor.embed_text(text.page_content)
+        document_vector = processor.process_document(path)
+        print(key)
+        print(text.page_content)
+    # return document_vector
     
 
 def hello():
     return "world"
+
 if __name__ == "__main__":
-    start()
+    start("/Users/m_nestes/Documents/off-gold-80-hmo-ca-en.pdf")
