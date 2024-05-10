1dc17e088562740df8f7150de1d12879d5743860
Ekaterina Shaustruk <es@banksiaglobal.com>
2024-05-10 11:43:13 +0200

    fix: add some info

--- ui/src/app/pages/welcome/welcome.component.ts
+++ ui/src/app/pages/welcome/welcome.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { SessionStorageService } from '../../service/localStorage.service';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
-  imports: [NzButtonModule, RouterLink],
+  imports: [NzButtonModule, RouterLink, NzTypographyModule],
 })
 export class WelcomeComponent {
   constructor(private localStorage: SessionStorageService) {}
